4f262fa7832bc13275f9bf12c3d5c67214328b29
Abdul Subhan <abduls.ug22.cs@nitp.ac.in>
2024-02-24 14:47:12 +0530

    Updated functions for WaterMark Verification

--- ML/extracttext.py
+++ ML/extracttext.py
@@ -1,15 +1,13 @@
+# extract_text.py
 import cv2
 import pytesseract
 
-# Load the image
-image = cv2.imread("output_imgs/modified_image.jpg")  # Replace "image_with_text.jpg" with the path to your image
+def extract_text(image_path):
+    image = cv2.imread(image_path)
 
-# Convert the image to grayscale
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Use pytesseract to extract text
-extracted_text = pytesseract.image_to_string(gray_image)
+    extracted_text = pytesseract.image_to_string(gray_image)
 
-# Print the extracted text
-print("Extracted Text:")
-print(extracted_text)
+    print("Extracted Text:")
+    print(extracted_text)
--- ML/textonimg.py
+++ ML/textonimg.py
@@ -1,31 +1,27 @@
 import cv2
 import numpy as np
-
+from facer import Person
 from facer import recognise
-image = cv2.imread("test_imgs\subhdiv.jpg")  
+# recognise()
+# apply_watermark.py
+import cv2
 
+def apply_watermark(image_path, text, output_path):
+    image = cv2.imread(image_path)
 
-text = "Abdul Subhan - 2206270"
-font = cv2.FONT_HERSHEY_SIMPLEX
-font_scale = 1
-font_thickness = 2
-font_color = (255, 255, 255)  
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    font_thickness = 2
+    font_color = (255, 255, 255)
 
+    text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
 
-text_size = cv2.getTextSize(text, font, font_scale, font_thickness)[0]
+    border_size = 50
+    image_with_border = cv2.copyMakeBorder(image, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT, value=(0, 0, 0))
 
+    text_position = ((image_with_border.shape[1] - text_size[0]) // 2, border_size - 10)
 
-border_size = 50
-image_with_border = cv2.copyMakeBorder(image, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+    cv2.putText(image_with_border, text, text_position, font, font_scale, font_color, font_thickness)
 
-
-text_position = ((image_with_border.shape[1] - text_size[0]) // 2, border_size - 10)  
-
-
-cv2.putText(image_with_border, text, text_position, font, font_scale, font_color, font_thickness)
-
-
-output_path = "output_imgs\modified_image.jpg"
-cv2.imwrite(output_path, image_with_border)
-
-print(f"Modified image saved to {output_path}")
+    cv2.imwrite(output_path, image_with_border)
+    print(f"Modified image saved to {output_path}")
