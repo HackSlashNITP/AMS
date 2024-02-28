34707476484f43ee37c0de3c76c14aad5bb8858b
Dev <devsa2067@gmail.com>
2024-02-28 23:36:46 +0530

    Bottom Navigation Bar is Added

---
+++ ams_flutter/lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // BottomNavBar Colors
+  static const Color navIconColor = Colors.white;
+  static const Color navActiveIconColor = Colors.white;
+  static const Color tabBackgroundColor = Colors.green;
+  static const Color navBackgroundColor = Colors.black;
+}
---
+++ ams_flutter/lib/core/constants/app_data.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class AppData {
+  static List<GButton> bottomNavItems = [
+    const GButton(
+      icon: Icons.home,
+      text: 'Home',
+    ),
+    const GButton(
+      icon: Icons.format_list_bulleted,
+      text: 'Search',
+    ),
+    const GButton(
+      icon: Icons.dashboard_outlined,
+      text: 'dashboard',
+    ),
+    const GButton(
+      icon: Icons.person,
+      text: 'Profile',
+    ),
+  ];
+}
