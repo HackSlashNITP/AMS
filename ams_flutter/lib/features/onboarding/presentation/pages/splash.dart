110ea98b9aca8356f10574e46e622075cbfc3c9f
SHAKTI <spmshakti@gmail.com>
2024-02-28 20:38:29 +0530

    implemantation of "Who are you page?"

--- ams_flutter/lib/features/onboarding/presentation/pages/splash.dart
+++ ams_flutter/lib/features/onboarding/presentation/pages/splash.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(
         const Duration(seconds: 3),
         () => Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const whoAreYou())));
+            MaterialPageRoute(builder: (context) =>  WhoAreYou())));
   }
 
   @override
