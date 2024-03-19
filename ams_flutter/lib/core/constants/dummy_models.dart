a1cb16f56aa366e1d044dc09bb7560f197e472d4
SHAKTI <spmshakti@gmail.com>
2024-03-19 13:31:50 +0530

    implementation of mark attendance page

--- ams_flutter/lib/core/constants/dummy_models.dart
+++ ams_flutter/lib/core/constants/dummy_models.dart
@@ -8,3 +8,42 @@ class AppInfo {
     {'title': 'Social Media', 'value': 'https://www.instagram.com'},
   ];
 }
+class DummyModels {
+  static List<Map<String, dynamic>> dummyStudents = [
+    {
+      'name': 'Shakti',
+      'rollno': '2206201',
+      'photoUrl': 'https://randomuser.me/api/portraits/men/1.jpg',
+    },
+    {
+      'name': 'Mahima',
+      'rollno': '2206201',
+      'photoUrl': 'https://randomuser.me/api/portraits/women/2.jpg',
+    },
+    {
+      'name': 'Piyush',
+      'rollno': '2206201',
+      'photoUrl': 'https://randomuser.me/api/portraits/men/3.jpg',
+    },
+    {
+      'name': 'Shradha',
+      'rollno': '2206201',
+      'photoUrl': 'https://randomuser.me/api/portraits/women/4.jpg',
+    },
+    {
+      'name': 'Dev',
+      'rollno': '2206201',
+      'photoUrl': 'https://randomuser.me/api/portraits/men/5.jpg',
+    },
+    {
+      'name': 'Utkarsh',
+      'rollno': '2206201',
+      'photoUrl': 'https://randomuser.me/api/portraits/men/6.jpg',
+    },
+       {
+      'name': 'Divyansh',
+      'rollno': '2206201',
+      'photoUrl': 'https://randomuser.me/api/portraits/men/7.jpg',
+    },
+  ];
+}
--- ams_flutter/lib/features/onboarding/presentation/pages/markAttendance.dart
+++ ams_flutter/lib/features/onboarding/presentation/pages/markAttendance.dart
@@ -7,6 +7,8 @@ import 'package:ams_flutter/features/onboarding/presentation/widgets/bottom_navb
 import 'package:ams_flutter/features/onboarding/presentation/widgets/text_field_widget.dart';
 import 'package:flutter/material.dart';
 import '../../../../core/constants/app_string.dart';
+import '../../../../core/constants/dummy_models.dart';
+import '../widgets/studentlist.dart';
 
 class MarkAttendance extends StatefulWidget {
   @override
@@ -15,6 +17,8 @@ class MarkAttendance extends StatefulWidget {
 
 class _MarkAttendanceState extends State<MarkAttendance> {
   late Size mq;
+
+  List<Map<String, dynamic>> dummyStudents = DummyModels.dummyStudents;
   @override
   Widget build(BuildContext context) {
     mq = MediaQuery.of(context).size;
@@ -36,29 +40,13 @@ class _MarkAttendanceState extends State<MarkAttendance> {
                   prefixIcon: Icon(Icons.search),
                   borderRadius: 40),
               StudentList(
-                students: [
-                  Student(
-                      name: 'John Doe',
-                      rollno: '2206201',
-                      photoUrl:
-                          'https://randomuser.me/api/portraits/men/1.jpg'),
-                  Student(
-                      name: 'Jane Smith',
-                      rollno: '2206201',
-                      photoUrl:
-                          'https://randomuser.me/api/portraits/women/2.jpg'),
-                  Student(
-                      name: 'Michael Johnson',
-                      rollno: '2206201',
-                      photoUrl:
-                          'https://randomuser.me/api/portraits/men/3.jpg'),
-                  Student(
-                      name: 'Emily Davis',
-                      rollno: '2206201',
-                      photoUrl:
-                          'https://randomuser.me/api/portraits/women/4.jpg'),
-                  // Add more students as needed
-                ],
+                students: dummyStudents
+                    .map((student) => Student(
+                          name: student['name'],
+                          rollno: student['rollno'],
+                          photoUrl: student['photoUrl'],
+                        ))
+                    .toList(),
               ),
             ],
           ),
@@ -67,7 +55,6 @@ class _MarkAttendanceState extends State<MarkAttendance> {
       bottomNavigationBar: BottomNavBar(),
     );
   }
-
   Widget _buildHeader(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -90,105 +77,24 @@ class _MarkAttendanceState extends State<MarkAttendance> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        _buildButton('B.Tech'),
-        _buildButton('Mechanical-B(2026)'),
-        _buildButton('13:30PM-14:30PM'),
+        BuildButton(
+          text: Text("B.Tech"),
+          color: Colors.black,
+          borderRadius: 16.0,
+        ),
+        BuildButton(
+          text: Text("B.Tech"),
+          color: Colors.black,
+          borderRadius: 16.0,
+        ),
+        BuildButton(
+          text: Text("13:30PM-14:30PM"),
+          color: Colors.black,
+          borderRadius: 16.0,
+        ),
       ],
     );
   }
 
-  Widget _buildButton(String text) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        child: Text(
-          text,
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-    );
-  }
 }
-
-class Student {
-  final String name;
-  final String photoUrl;
-  final String rollno;
-
-  Student({required this.name, required this.rollno, required this.photoUrl});
-}
-
-class StudentList extends StatelessWidget {
-  final List<Student> students;
-
-  const StudentList({required this.students});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      itemCount: students.length,
-      itemBuilder: (context, index) {
-        return _buildListItem(students[index]);
-      },
-    );
-  }
-
-  Widget _buildListItem(Student student) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          CircleAvatar(
-            backgroundImage: NetworkImage(student.photoUrl),
-          ),
-          SizedBox(width: 16),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(student.name),
-              Text(student.rollno),
-            ],
-          ),
-          BuildButton(color: Colors.green,text: Text("Present")),
-          BuildButton(color: Colors.red,text: Text("Absent")),
-        ],
-      ),
-    );
-  }
-  
-}
-class BuildButton extends StatelessWidget {
-  final Text text;
-  final Color color;
-  const BuildButton({
-    Key? key,
-    required this.text,
-    required this.color,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 4, horizontal: 10),
-        decoration: BoxDecoration(
-          color: color, 
-          borderRadius: BorderRadius.circular(5),
-        ),
-        child:Text(
-          text.data!, 
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-    );
-  }
-}
-
 
