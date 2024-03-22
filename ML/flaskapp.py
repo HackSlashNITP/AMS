ee4e2b5841be90901aec4decf2b971491665d12f
Autobot37 <andyrights37@gmail.com>
2024-03-22 15:29:45 +0530

    working flask

--- ML/flaskapp.py
+++ ML/flaskapp.py
@@ -1,5 +1,5 @@
 from facer import Person
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from textonimg import apply_watermark
 from flask_cors import CORS
 
@@ -7,11 +7,18 @@ app = Flask(__name__)
 CORS(app)  
 
 def get_persons():
+    ##when database will be connected here we will fetch all persons information.
     persons = []
+    persons.append(Person("dummy","9696","test_imgs/div.jpg"))
     return persons
 
-persons = get_persons
+persons = get_persons()
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+    
 @app.route('/recognize', methods=['POST'])
 def recognize_person():
     if 'file' not in request.files:
@@ -26,9 +33,10 @@ def recognize_person():
     for person in persons:
         result = person.recognise("temp.jpg")        
         if result is not None:
+            print(result)
             info = result.name + " " + result.roll_no
             apply_watermark("temp.jpg", info, f'output_imgs/{info}.jpg')
-            return jsonify({'result': 'success', 'image_path': f'output_imgs/{info}.jpg'})
+            return jsonify({'result': 'success', 'image_path': f'output_imgs/{info}.jpg','name':person.name,'roll_no':person.roll_no})
     
     return jsonify({'result': 'failure', 'message': 'Not matched'}), 404
 
