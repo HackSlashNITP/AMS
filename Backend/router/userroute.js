63647ed01c35557ce20338e5635a3c5e04309bb8
Dev <devsa2067@gmail.com>
2024-04-23 15:26:28 +0530

    erorr handling added

--- Backend/router/userroute.js
+++ Backend/router/userroute.js
@@ -3,6 +3,8 @@ const usercontroller = require("../controllers/userc");
 
 const router = express.Router();
 
+// REQUESTS
+
 router.post("/register", usercontroller.createUser);
 router.get("/:userId", usercontroller.getUserById);
 router.put("/:userId", usercontroller.updateUser);
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,21 @@
+  // IMPORTS
 const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = 3000;
-
-const app = express();
-app.use(bodyParser.json());
-
 const userroute = require("./router/userroute");
 const adminroute = require("./router/adminroute");
 
+  // INIT
+const app = express();
+app.use(bodyParser.json());
+
+  // ROUTES
+
 app.use("/admin", adminroute);
 app.use("/user", userroute);
+
+
+// PORT LISTENING
 
 app.listen(PORT, () => {
   console.log("Server is running on ", PORT);
