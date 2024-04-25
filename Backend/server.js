a525a11ae2ccdad8555b2641f12f6b3f0dbca27b
SHAKTI <spmshakti@gmail.com>
2024-04-25 10:24:07 +0530

    Connect Database

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,17 @@
   // IMPORTS
 const express = require("express");
 const bodyParser = require("body-parser");
-const PORT = 3000;
+// const PORT = 3000;
+const dotenv=require('dotenv');
+dotenv.config({path:'.env-local'});
+const PORT= process.env.PORT || '3000';
 const userroute = require("./router/userroute");
 const adminroute = require("./router/adminroute");
 
   // INIT
 const app = express();
 app.use(bodyParser.json());
+app.use(express.json());
 
   // ROUTES
 
