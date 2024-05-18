5fab17cb7598adede3814856ba67d5541f2cfd42
Utkarsh Ranjan <128368184+Yashrajdps13@users.noreply.github.com>
2024-05-18 21:28:16 +0530

    Update classroomroute.js

--- Backend/router/classroomroute.js
+++ Backend/router/classroomroute.js
@@ -3,7 +3,7 @@ const classroomcontroller = require("../controllers/classroomc");
 const router = express.Router();
 // show all data
 // router.post("/create", classroomcontroller.createClassroom);
-router.get("/allclassroom", classroomcontroller.getAllClassrooms);
+router.get("/classrooms", classroomcontroller.getAllClassrooms);
 router.get("/:classroomId", classroomcontroller.getClassroomById);
 // router.delete("/:classroomId", classroomcontroller.deleteClassroomById);
 
