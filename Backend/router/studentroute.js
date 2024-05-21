eab4c0b42dc0619d0b78932f98995ee1ab9dc2c5
Utkarsh Ranjan <128368184+Yashrajdps13@users.noreply.github.com>
2024-05-21 20:20:29 +0530

    Update studentroute.js

--- Backend/router/studentroute.js
+++ Backend/router/studentroute.js
@@ -6,6 +6,6 @@ router.post("/register", studentcontroller.createStudent);
 router.get("/student", studentcontroller.getStudentById);
 router.get("/classrooms", studentcontroller.getAllClassrooms);
 router.put("/updateDetails", studentcontroller.updateStudent);
-router.put("/addClassrooms", studentcontroller.addClassroom);
+router.put("/concatClassroomId", studentcontroller.concatClassroomId);
 router.delete("/:userId", studentcontroller.deleteStudent);
 module.exports = router;
